54eea159181ffa63bd6dfcbe54914776d9079dfe
zhandamm <sai.eduself@gmail.com>
2023-03-26 13:23:32 +0300

    stage 1

--- Main.kt
+++ Main.kt
@@ -1,7 +1,27 @@
 package tasklist
 
 fun main() {
-    // write your code here
+    val taskContainer = HashMap<Int, String>()
+    var taskNumber = 1
+    println("Input the tasks (enter a blank line to end):")
+    while (true) {
+        val task = readln().trim()
+        if (taskNumber == 1 && task == "") {
+            println("No tasks have been input")
+        }
+        if (task == "") {
+            break
+        }
+        taskContainer[taskNumber] = task
+        taskNumber++
+    }
+    printTasks(taskContainer)
+}
+
+fun printTasks(tasks: HashMap<Int, String>) {
+    for ((key, value) in tasks) {
+        println(key.toString().padEnd(3, ' ') + value)
+    }
 }
 
 
