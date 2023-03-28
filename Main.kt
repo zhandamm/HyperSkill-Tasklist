6d876f443a5e81efc0e1434cd8045d0a9e5e937d
zhandamm <sai.eduself@gmail.com>
2023-03-28 23:58:55 +0300

    stage 2

--- Main.kt
+++ Main.kt
@@ -1,23 +1,71 @@
 package tasklist
 
-fun main() {
-    val tasks = mutableListOf<String>()
-    println("Input the tasks (enter a blank line to end):")
-    while (true) {
-        val task = readln().trim()
-        if (tasks.isEmpty() && task.isEmpty()) {
-            println("No tasks have been input")
-            break
+class Task(private var text: String = "") {
+    private val tasks = mutableListOf<Task>()
+
+    //TODO провести рефакторинг этой функции
+    fun addTaskList() {
+        val task = Task()
+
+        while (true) {
+            val input = readln().trim()
+
+            if (input == "" && task.text.trim().isNotEmpty()) {
+                tasks.add(task)
+                break
+            }
+
+            task.text += if (task.text.isEmpty()) input else "\n   $input"
+            if (input == "" && task.text.trim().isEmpty()) {
+                println("The task is blank")
+                break
+            }
         }
-        if (task.isEmpty()) break
-        tasks.add(task)
     }
-    printTasks(tasks)
+
+    fun printTaskList() {
+        if (tasks.isEmpty()) {
+            println("No tasks have been input")
+        } else printTasks(tasks)
+    }
+
+    override fun toString(): String {
+        return text
+    }
+}
+//TODO Сделать чтобы пустой ввод не добавлялся
+
+fun chooseAction(task: Task) {
+    while (true) {
+        println("Input an action (add, print, end):")
+        when (readln()) {
+            "add" -> {
+                println("Input a new task (enter a blank line to end):")
+                task.addTaskList()
+            }
+
+            "print" -> task.printTaskList()
+
+            "end" -> {
+                println("Tasklist exiting!")
+                break
+            }
+
+            else -> {
+                println("The input action is invalid")
+                continue
+            }
+        }
+    }
 }
 
-fun printTasks(tasks: List<String>) {
+fun printTasks(tasks: MutableList<Task>) {
     tasks.forEachIndexed { index, task ->
-        println("${index + 1}".padEnd(3, ' ') + task)
+        println("${index + 1}".padEnd(3, ' ') + task + "\n")
     }
 }
 
+fun main() {
+    val task = Task()
+    chooseAction(task)
+}
